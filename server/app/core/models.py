257ae31790f775a10d856fb0b278b05b44a27ed1
Michal Janecek <michal.janecek@gmail.com>
2021-02-23 20:16:24 +0100

    Added simplest entity to database

--- server/app/core/models.py
+++ server/app/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Tag(models.Model):
+    """Tag to be used for a recipe"""
+    name = models.CharField(max_length=255)
+   
+
+    def __str__(self):
+        return self.name
