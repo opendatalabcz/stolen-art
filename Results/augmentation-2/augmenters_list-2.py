bbabc1858032108f5d4cbab9a7050c074cfe7fcf
Michal Janecek <michal.janecek@gmail.com>
2021-05-09 02:57:18 +0200

    results '

--- Results/augmentation-2/augmenters_list-2.py
+++ Results/augmentation-2/augmenters_list-2.py
@@ -1,23 +1,25 @@
 # specify the augmentors to be used
 augmenters_list = [
     
-      
-    iaa.GaussianBlur(sigma=(0, 5.0)), # blur images with a sigma of 0 to 5.0
-   
+    # Gaussian blur images with a sigma from range
+    iaa.GaussianBlur(sigma=(0, 5.0)), 
+    
+    # apply JpegCompression on some images
+    # degrade the quality of images by JPEG-compressing them. 
     iaa.Sometimes(0.50,
-                iaa.JpegCompression(compression=(0, 30)) ),# degrade the quality of images by JPEG-compressing them.  
+                iaa.JpegCompression(compression=(0, 30)) ), 
         
     iaa.Affine(
-        scale={"x": (0.4, 1), "y": (0.4, 1)}, # zooming out
+        scale=(0.3,2), # zooming in/out
         translate_percent={"x": (-0.05, 0.05), "y": (-0.05, 0.05)}, # shifting on axis
         rotate=(-50, 50), # rotating
         ),
-
+    
+    # changes contrast
     iaa.LinearContrast((0.75, 1.5)),
         
+    # sometimes cut a square hole in an image and fill it with noise
     iaa.Sometimes(0.25,
-        iaa.Cutout(fill_mode="gaussian", fill_per_channel=True) ), # cutout a square, fill it with noise
-                                                                   # algo should not rely on just one area of keypoints
-        
+        iaa.Cutout(fill_mode="gaussian", fill_per_channel=True) ),
         
     ]
